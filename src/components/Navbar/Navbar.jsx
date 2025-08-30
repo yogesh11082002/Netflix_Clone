22ec69e5e3498cca983e7cd9e34814a2c54cc302
Yogesh <yogeshthakur938920@gmail.com>
2025-08-30 09:25:58 +0530

    changes for navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,7 +40,8 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-right">
-          <img src={search_icon} alt="search icon" className="icons" />
+          <img src={search_icon} alt="search icon" className="icons search" />
+
           <p>Children</p>
           <img src={bell_icon} alt="bell icon" className="icons" />
 
